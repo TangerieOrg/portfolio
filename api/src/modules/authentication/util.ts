1be65fce04b0db2074f3859b4ec42ef46e63877a
Tangerie <josh.cavill01@gmail.com>
2022-09-18 15:09:18 +1000

    A

--- api/src/modules/authentication/util.ts
+++ api/src/modules/authentication/util.ts
@@ -4,6 +4,7 @@ import { LoginResponse, User } from "./types";
 const AUTH_URL = process.env.AUTH_SERVICE_URL ?? "http://localhost:8000";
 
 export async function getUser(cookie : string) {
+    console.log("Attemping to get user", cookie, AUTH_URL);
     return await axios.get(`${AUTH_URL}/`, {
         data: { cookie }
     }).then(x => x?.data as User | undefined);
